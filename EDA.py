8b18f62721e0c7f82842364e2f0fceab0bb350b2
Robin <rvdb7345@gmail.com>
2021-09-22 08:56:11 +0200

    First files added. Code extraction of the tar file.

---
+++ EDA.py
@@ -0,0 +1,12 @@
+""" This file is used for creating an overview of the information contained in all the different sessions of the UN
+speeches
+
+Authors:
+
+"""
+
+import csv
+
+import numpy as np
+import pandas as pd
+
