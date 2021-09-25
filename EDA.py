d7810862c95f42227e7841e941f9631fdad0d722
Lan Chu <LCU1812@fmo.nl>
2021-09-26 00:37:47 +0200

    Included data cleanup routines for happiness and speech dfs

--- EDA.py
+++ EDA.py
@@ -195,6 +195,65 @@ def remove_line_number(speech):
     
     return speech
 
+
+def happiness_df_cleanup(happiness_df):    
+    '''
+    Creating a dictionary to map the wrong countries name in
+    happiness dataframe with correct country name. This is because 
+    we need to merge happiness dataframe with speeches dataframe 
+    using country name. And we found cases where country names are
+    not the same between these two data set. 
+    See function check_countryname_consistency as well.
+    '''
+    
+    country_mapping = {
+        'Vietnam' : 'Viet Nam',
+        'Moldova' :  'Republic of Moldova',
+        'Laos' :  "Lao People's Democratic Republic",
+        'Somaliland region' :  'Somalia',
+        'Kosovo': None,
+        'Taiwan Province of China': None,
+        'United Kingdom' :  'United Kingdom of Great Britain and Northern Ireland',
+         'United States' :  'United States of America',
+        'South Korea' : 'Republic of Korea',
+        'Ivory Coast' : 'Côte d’Ivoire',
+        'Czech Republic' : 'Czechia',
+        'Swaziland' :  'Eswatini',
+         'Russia' : 'Russian Federation',
+         'Hong Kong S.A.R. of China' : 'China-Hong Kong Special Administrative Region',
+         'Palestinian Territories' :  'State of Palestine',
+         'Tanzania' :  'United Republic of Tanzania',
+         'Syria' : 'Syrian Arab Republic',
+         'North Cyprus' :  'Cyprus',
+         'Bolivia' : 'Bolivia (Plurinational State of)',
+         'Congo (Kinshasa)' : 'Democratic Republic of the Congo',
+        'Venezuela': 'Venezuela (Bolivarian Republic of)',
+         'Iran': 'Iran (Islamic Republic of)',
+         'Congo (Brazzaville)' :  'Congo'}
+    # Replace the country names in happiness dataframe with the correct country names in df-codes. 
+    happiness_df = happiness_df.reset_index().replace({'Country name': country_mapping} ).set_index(['Country name', 'year'])
+    
+    return happiness_df
+
+def speeches_df_cleanup(speeches_df):
+    '''
+    Replace YDYE (yemen) and POR (Portugal) with correct iso_alpho3 code.
+    The remaing 'DDR', 'YUG', 'EU', 'CSK' are not considered countries by 
+    the UN or don't exist anymore, so we can consider removing them out of
+    dataset because we don't have happiness data for these "countries".
+    '''
+    speeches_df['country'] = speeches_df['country'].str.replace('YDYE','YEM').replace('POR','PRT')
+    return speeches_df
+
+def check_countryname_consistency(happiness_df, df_codes):
+    happiness_countries = set(happiness_df.reset_index()['Country name'].unique())
+    iso_countries = set(df_codes['Country or Area'])
+    print(f"The following countries in happiness_df do not appear in iso_countries: {happiness_countries - iso_countries}")
+
+def check_isocode_consistency(speeches_df,df_codes):
+    speech_codes = set(speeches_df['country'].unique())
+    iso_codes = set(df_codes['ISO-alpha3 Code'].unique())
+    print(f"The following codes in speeches_df do not appear in df_codes: {speech_codes - iso_codes}")
 if __name__ == '__main__':
 
     # True --> run preprocessing and save the results, False --> just do the data analysis with your previously saved
@@ -244,18 +303,26 @@ if __name__ == '__main__':
                                                   },
                                                  ignore_index=True)
 
-        # add the country names to the dataframe
+        # read in country codes and happiness data
         df_codes = pd.read_csv('UNSD — Methodology.csv', delimiter=',')
+        happiness_df = pd.read_excel('DataPanelWHR2021C2.xls', index_col=[0, 1])
+
+        # check if country names and iso codes are consistent before merging
+        check_isocode_consistency(speeches_df, df_codes)
+        check_countryname_consistency(happiness_df, df_codes)
+
+        # cleanup dataframes before merging
+        speeches_df = speeches_df_cleanup(speeches_df)
+        happiness_df = happiness_df_cleanup(happiness_df)
+
         speeches_df = speeches_df.merge(df_codes, how='left', left_on='country', right_on='ISO-alpha3 Code')
 
-        # add the happiness dataframe to the
-        happiness_df = pd.read_excel('DataPanelWHR2021C2.xls', index_col=[0, 1])
+        # add the happiness dataframe to the  
+
         speech_happi_merged_df = pd.merge(speeches_df, happiness_df, how='left',
                                           left_on=['year', 'Country or Area'], right_on=['year', 'Country name'])
 
         speech_happi_merged_df.to_csv('preprocessed_dataframe.csv')
-
-
 
     # get into the exploration after reading the saved file
     speeches_df = pd.read_csv('preprocessed_dataframe.csv')
